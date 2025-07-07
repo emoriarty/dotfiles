1371f7d2a8ceaa5d9d9fd1ca285d9c61e84fca98
Enrique Arias <emoriarty81@gmail.com>
2025-07-07 23:56:39 +0200

    cleanup

--- _old_stuff/install/vscode.sh
+++
@@ -1,4 +0,0 @@
-code --force --install-extension esbenp.prettier-vscode
-code --force --install-extension bierner.color-info
-code --force --install-extension vscodevim.vim
-code --force --install-extension dbaeumer.vscode-eslint
