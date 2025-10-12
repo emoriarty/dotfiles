d2533e0059b49b0f7f9dbf5db11c080c518f67be
Enrique Arias <emoriarty81@gmail.com>
2025-10-12 18:39:52 +0200

    fix(nvim): none-ls rubocop support

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -3,13 +3,40 @@ return {
   config = function()
     local null_ls = require("null-ls")
 
+    -- Helper function to check if Gemfile exists - using vim.fn.findfile which searches up the directory tree
+    local function has_gemfile()
+      return vim.fn.findfile("Gemfile", ".;") ~= ""
+    end
+
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.diagnostics.erb_lint,
-        null_ls.builtins.diagnostics.rubocop,
-        null_ls.builtins.formatting.rubocop,
+        null_ls.builtins.diagnostics.rubocop.with({
+          command = "bundle",
+          args = { "exec", "rubocop", "--format", "json", "--force-exclusion", "--stdin", "$FILENAME" },
+          runtime_condition = function()
+            return has_gemfile()
+          end,
+        }),
+        null_ls.builtins.diagnostics.rubocop.with({
+          runtime_condition = function()
+            return not has_gemfile()
+          end,
+        }),
+        null_ls.builtins.formatting.rubocop.with({
+          command = "bundle",
+          args = { "exec", "rubocop", "--auto-correct", "-f", "quiet", "--stderr", "--stdin", "$FILENAME" },
+          runtime_condition = function()
+            return has_gemfile()
+          end,
+        }),
+        null_ls.builtins.formatting.rubocop.with({
+          runtime_condition = function()
+            return not has_gemfile()
+          end,
+        }),
       },
     })
 
