7879c4693cf4c3a207cda0264e714fe3597ea895
Enrique Arias <emoriarty81@gmail.com>
2025-10-13 15:50:01 +0200

    fix(nvim): theme styntax highlighting

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -42,5 +42,12 @@
 return {
   "scottmckendry/cyberdream.nvim",
   lazy = false,
-  priority = 1000
+  priority = 1000,
+  config = function()
+    require("cyberdream").setup({
+      transparent = false,
+      italic_comments = true,
+    })
+    vim.cmd("colorscheme cyberdream")
+  end
 }
