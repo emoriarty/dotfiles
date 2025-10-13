23a0c7bcbcbdf47d0d3d5e05f1cfd5615939a4ce
Enrique Arias <emoriarty81@gmail.com>
2025-10-13 15:50:24 +0200

    fix(nvim): vim-test works in non-tmux env

--- nvim/.config/nvim/lua/plugins/vim-test.lua
+++ nvim/.config/nvim/lua/plugins/vim-test.lua
@@ -1,12 +1,28 @@
 return {
-  "vim-test/vim-test",
-  dependencies = {
+  {
     "preservim/vimux",
+    lazy = false,
+    cond = function()
+      return vim.env.TMUX ~= nil
+    end,
   },
-  vim.keymap.set('n', '<leader>t', ':TestNearest<CR>'),
-  vim.keymap.set('n', '<leader>T', ':TestFile<CR>'),
-  vim.keymap.set('n', '<leader>a', ':TestSuite<CR>'),
-  vim.keymap.set('n', '<leader>l', ':TestLast<CR>'),
-  vim.keymap.set('n', '<leader>g', ':TestVisit<CR>'),
-  vim.cmd("let test#strategy = 'vimux'"),
+  {
+    "vim-test/vim-test",
+    lazy = false,
+    dependencies = { "preservim/vimux" },
+    config = function()
+      vim.keymap.set('n', '<leader>t', ':TestNearest<CR>')
+      vim.keymap.set('n', '<leader>T', ':TestFile<CR>')
+      vim.keymap.set('n', '<leader>a', ':TestSuite<CR>')
+      vim.keymap.set('n', '<leader>l', ':TestLast<CR>')
+      vim.keymap.set('n', '<leader>g', ':TestVisit<CR>')
+
+      -- Use vimux when in tmux, neovim terminal otherwise
+      if vim.env.TMUX ~= nil then
+        vim.cmd("let test#strategy = 'vimux'")
+      else
+        vim.cmd("let test#strategy = 'neovim'")
+      end
+    end,
+  },
 }
